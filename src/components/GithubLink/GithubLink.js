8c6898554a35cb47331449ecce604be4382f1702
Bilal Ahmed Khan <99674150+BilalAhmed-358@users.noreply.github.com>
2023-06-28 13:10:37 +0500

    Fixed typos GithubLink.js

--- src/components/GithubLink/GithubLink.js
+++ src/components/GithubLink/GithubLink.js
@@ -5,7 +5,7 @@ import './GithubLink.css'
 export default function GithubLink() {
     return (
         <>
-            <div className="github-link"><p>You can check the  Github <BsGithub /> repository of this project by click on this <a href="https://github.com/BilalAhmed-358/Analysis_of_Algorithms_Project" target='_blank'>Link.</a></p></div>
+            <div className="github-link"><p>You can checkout the  Github <BsGithub /> repository of this project by click on this <a href="https://github.com/BilalAhmed-358/Analysis_of_Algorithms_Project" target='_blank'>Link.</a></p></div>
             
         </>
     )
